da6240e30ac9472bf13db4afafbcc6dbd33a0b16
Michael Ceballos <ceblance@aol.com>
2021-07-04 18:08:06 -0700

    added more js

--- script.js
+++ script.js
@@ -2,10 +2,52 @@ const characterAmountRange = document.getElementById
 ('characterAmountRange')
 const characterAmountNumber = document.getElementById
 ('characterAmountNumber')
+const includeUppercaseElement = document.getElementById
+('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+const form = document.getElementById('passwordGeneratorForm')
+
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const UPPERCASE_CHAR_CODES = arrayFromLowtoHigh(65, 90)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
 
 // Add event listener 
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
 characterAmountRange.addEventListener('input', syncCharacterAmount)
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+  const password = generatePassword(characterAmount, includeUppercase,
+  includeNumbers, includeSymbols)
+})
+
+function generatePassword(characterAmount, includeUppercase,
+  includeNumbers, includeSymbols) {
+    console.log(LOWERCASE_CHAR_CODES)
+  }
+
+  function arrayFromLow(low, high) {
+    const array = []
+    for (let i = low; i <= high; i++) {
+      array.push(i)
+    }
+    return array
+  }
+  
+
+
 
 function syncCharacterAmount(e) {
   const value = e.target.value
@@ -15,26 +57,19 @@ function syncCharacterAmount(e) {
 
 
 
-function generatePassword(){
+
   //1. seperate all characters
   //2. ask the user which characters they want and create a group of those characters only
  //3. select random characters from the creative group of characters
  //4. return the password
  
-  return "pancakes"
-}
+  
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
